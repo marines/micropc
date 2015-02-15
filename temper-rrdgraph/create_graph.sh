58aff7d28d4099a7eaf1d014d9016599b95e5399
Mariusz Kujawski <email@mariuszkujawski.pl>
2015-02-15 13:53:24 +0100

    added daily temperature graph

--- temper-rrdgraph/create_graph.sh
+++ temper-rrdgraph/create_graph.sh
@@ -7,3 +7,14 @@ rrdtool graph /home/marines/repos/micropc/temper-rrdgraph/graphs/$1.png \
 --vertical-label "temperature (°C)" \
 DEF:temp=/home/marines/repos/micropc/temper-rrdgraph/databases/$1.rrd:temp:MAX \
 LINE1:temp#ff0000:"temp"
+
+
+rrdtool graph /home/marines/repos/micropc/temper-rrdgraph/graphs/$1-daily.png \
+-w 458 -h 120 -a PNG \
+--slope-mode \
+--start -86400 --end now \
+--vertical-label "temperature (°C)" \
+DEF:temp=/home/marines/repos/micropc/temper-rrdgraph/databases/$1.rrd:temp:MAX \
+LINE1:temp#ff0000:"temp"
+
+
