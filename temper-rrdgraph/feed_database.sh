d82627b233e6e3f2442edbfb0766a0d6845ff87a
Mariusz Kujawski <email@mariuszkujawski.pl>
2015-02-15 14:00:33 +0100

    prevent from writing wrong values in case of USB read error

--- temper-rrdgraph/feed_database.sh
+++ temper-rrdgraph/feed_database.sh
@@ -2,7 +2,12 @@
 
 TEMP=`/home/marines/repos/micropc/temper-rrdgraph/bin/pcsensor | cut -d' ' -f 5 | cut -d'C' -f 1`
 
-echo rrdtool update /home/marines/repos/micropc/temper-rrdgraph/databases/$1.rrd N:$TEMP
+# don't update db if there was an error during read
+re='^-?[0-9]+([.][0-9]+)?$'
+if ! [[ $TEMP =~ $re ]] ; then
+	exit 1
+fi
+
 rrdtool update /home/marines/repos/micropc/temper-rrdgraph/databases/$1.rrd N:$TEMP
 
 
